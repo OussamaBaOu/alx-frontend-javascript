ce8004bce00b5ba408c2fa610d81cf3cddd542ab
O.B.O <125463724+OussamaBaOu@users.noreply.github.com>
2024-01-03 06:27:37 +0100

    11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,4 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
-const emp = { [departmentName]: employees };
+  const emp = { [departmentName]: employees };
   return emp;
 }
